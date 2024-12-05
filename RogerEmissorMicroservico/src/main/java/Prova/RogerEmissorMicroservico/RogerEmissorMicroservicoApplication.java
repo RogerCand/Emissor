2f73a13e8e84932452a2aee1b3426f856fabef38
ADS 2024.1N <36129382024.1N@senacrjedu.local>
2024-12-05 19:11:32 -0300

    prova

---
+++ RogerEmissorMicroservico/src/main/java/Prova/RogerEmissorMicroservico/RogerEmissorMicroservicoApplication.java
@@ -0,0 +1,13 @@
+package Prova.RogerEmissorMicroservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RogerEmissorMicroservicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RogerEmissorMicroservicoApplication.class, args);
+	}
+
+}
---
+++ RogerEmissorMicroservico/src/test/java/Prova/RogerEmissorMicroservico/RogerEmissorMicroservicoApplicationTests.java
@@ -0,0 +1,13 @@
+package Prova.RogerEmissorMicroservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RogerEmissorMicroservicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
